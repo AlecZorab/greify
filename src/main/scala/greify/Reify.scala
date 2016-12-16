cae9f8dd40024f216896025e117cfb2511a251a0
azorab <alec.zorab@scotiabank.com>
2016-12-16 18:19:29 +0000

    Use IdentityHashMap in place of StableNames hack

--- src/main/scala/greify/Reify.scala
+++ src/main/scala/greify/Reify.scala
@@ -1,21 +1,13 @@
 package greify
 
+import java.util
+
 import scala.language.higherKinds
 import cats._
 import cats.implicits._
-import collection.mutable.AnyRefMap
 
 case class Graph[F[_]](edges: Array[F[Int]]) {
   override def toString = s"Graph(${edges.mkString(", ")})"
-}
-
-case class StableName(val a: AnyRef) {
-  override def hashCode = System.identityHashCode(a)
-  override def toString = s"StableName@$hashCode"
-  override def equals(other: Any) = other match {
-    case that: StableName => that.a eq this.a
-    case _                => false
-  }
 }
 
 trait MuRef[A] {
@@ -26,18 +18,18 @@ trait MuRef[A] {
 object MuRef {
   type Aux[A, B[_]] = MuRef[A] { type Out[X] = B[X] }
 
-  def reifyGraph[S <: AnyRef, F[_]](s: S)(implicit ev: MuRef.Aux[S, F], ct: reflect.ClassTag[F[Int]]): Graph[F] = {
-    val stableNames = AnyRefMap.empty[StableName, Int]
+  def reifyGraph[S, F[_]](s: S)(implicit ev: MuRef.Aux[S, F], ct: reflect.ClassTag[F[Int]]): Graph[F] = {
+    import scala.collection.JavaConverters._
+    val positions = new util.IdentityHashMap[S, Int]().asScala
     var indices     = List.empty[(Int, F[Int])]
     var ctr         = 0
     def findNodes(s: S): Id[Int] = {
-      val name = StableName(s)
-      stableNames get name match {
+      positions get s match {
         case Some(i) => i
         case None =>
           val i = ctr
           ctr += 1
-          stableNames(name) = i
+          positions(s) = i
           val res = ev.mapDeRef(findNodes, s)
           indices ::= (i, res)
           i
